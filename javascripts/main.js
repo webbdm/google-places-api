29e52a60abbf886551da66ab0fff3a239236d799
webbdm <gwebb762@gmail.com>
2017-04-24 21:41:20 -0500

    fixed url

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,19 +5,20 @@ $(document).ready(function() {
     $("body").on("click", "li", (e) => {
         //console.log(e.target.innerHTML);
 
-        loadPlaces(e.target.innerHTML).then((data) =>{
-        	console.log(data);
-        }).catch((error) =>{
-        	console.log(error);
-        });
+        loadPlaces(e.target.innerHTML).then((data) => {
+            console.log(data);
+        }).catch((error) => {
+            console.log(error);
+        })
+
     });
 
     const loadPlaces = (dropdownType) => {
-        return new Promise ((resolve, reject) => {
-        $.ajax(`"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=36.174465,-86.767960&radius=500&type=${dropdownType}&keyword=cruise&key=${apiKey}`)
-        .done((data) => resolve(data))
-        .fail((error) => reject(error));
-    	});
+        return new Promise((resolve, reject) => {
+            $.ajax(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=36.174465,-86.767960&radius=500&type=${dropdownType}&key=${apiKey}`)
+                .done((data) => resolve(data))
+                .fail((error) => reject(error));
+        });
     };
 
 });
