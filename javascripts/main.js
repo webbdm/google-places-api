9b2cd23b995aa938297639f80f093bff88f9b7ed
webbdm <gwebb762@gmail.com>
2017-04-25 19:49:02 -0500

    Added writeToDom function for places and their addresses

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,20 +3,53 @@ $(document).ready(function() {
     const apiKey = "";
 
     $("body").on("click", "li", (e) => {
-        //console.log(e.target.innerHTML);
 
-        loadPlaces(e.target.innerHTML).then((data) => {
-            console.log(data);
+        loadPlaces(e.target.innerHTML).then((results) => {
+            //console.log(results);
+            print(results);
         }).catch((error) => {
             console.log(error);
         })
 
     });
 
+    $("body").on("click", ".place", (e) => {
+        console.log(e.target.id);
+        let place_id = e.target.id;
+        loadDetail(place_id).then((result) => {
+            console.log(result);
+            writeAddressToDom(result.formatted_address);
+        });
+    });
+
+    const loadDetail = (place_id) => {
+        return new Promise((resolve, reject) => {
+            $.ajax(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${place_id}&key=${apiKey}`)
+                .done((data) => resolve(data.result))
+                .fail((error) => reject(error));
+        })
+    }
+
+    const print = (places) => {
+        let outputString = "";
+
+        places.forEach((place, i) => {
+            //console.log(place);
+            outputString += `<a href="#"><div class="place" id="${place.place_id}">${place.name}</div></a>`;
+        });
+        $("#main").html(outputString);
+    }
+
+    const writeAddressToDom = (address) => {
+        let outputString = `<div>${address}</div>`;
+        $("#addresses").append(outputString);
+    }
+
+
     const loadPlaces = (dropdownType) => {
         return new Promise((resolve, reject) => {
-            $.ajax(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=36.174465,-86.767960&radius=500&type=${dropdownType}&key=${apiKey}`)
-                .done((data) => resolve(data))
+            $.ajax(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=36.174465,-86.767960&radius=5000&type=${dropdownType}&key=${apiKey}`)
+                .done((data) => resolve(data.results))
                 .fail((error) => reject(error));
         });
     };
